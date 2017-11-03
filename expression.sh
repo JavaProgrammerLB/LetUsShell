cb59b390894dfe997556c97ebf299e4e074341d7
刘备 <admin@yitianyigexiangfa.com>
2017-11-03 09:28:58 +0800

    operation expression

--- expression.sh
+++ expression.sh
@@ -2,9 +2,11 @@ echo `expr 1 + 1`
 echo `expr 10 \* 200`
 echo `expr 1 - 2`
 echo `expr 100 / 10`
+echo `expr 10 % 6`
 
 # mac expr
 echo $((100 + 100))
 echo $((100 * 1000))
 echo $((100 / 10))
 echo $((100 - 1))
+echo $((10 % 6))
